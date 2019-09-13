2efbe3da3d29a8dfd4d8c44147e2ad7d03fe5c9d
LHDD <elhadi98@gmail.com>
2019-09-13 22:49:33 +0100

    Updates

---
+++ precache-manifest.ba0731b0ff13d43c52edf9dabbb11fc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15faecaad439ce2202ffe859e45f176d",
+    "url": "/TD-React-Challenge-Test-1/index.html"
+  },
+  {
+    "revision": "9440e7a079de7c7b883e",
+    "url": "/TD-React-Challenge-Test-1/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "367de9a89f0bc0d3e4e5",
+    "url": "/TD-React-Challenge-Test-1/static/js/2.08ff1b70.chunk.js"
+  },
+  {
+    "revision": "9440e7a079de7c7b883e",
+    "url": "/TD-React-Challenge-Test-1/static/js/main.dac88f53.chunk.js"
+  },
+  {
+    "revision": "07b441305761f547a5f1",
+    "url": "/TD-React-Challenge-Test-1/static/js/runtime~main.52067aab.js"
+  }
+]);
