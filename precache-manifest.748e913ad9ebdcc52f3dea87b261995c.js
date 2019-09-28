a0fcf43c98e4ca1be910c4fb51b2048412d17aea
LHDD <elhadi98@gmail.com>
2019-09-28 10:56:16 +0100

    Updates

---
+++ precache-manifest.748e913ad9ebdcc52f3dea87b261995c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6fc94b886e0d5e0b8e9a4d464fcd4e",
+    "url": "/AQr/272ccc514fb8cdb707c7.worker.js"
+  },
+  {
+    "revision": "e49907237548dad969a44a314db21579",
+    "url": "/AQr/index.html"
+  },
+  {
+    "revision": "85ba864cd5173c3f372a",
+    "url": "/AQr/static/css/main.df579641.chunk.css"
+  },
+  {
+    "revision": "674d8f7115d8ba3c6b6a",
+    "url": "/AQr/static/js/2.962d271a.chunk.js"
+  },
+  {
+    "revision": "85ba864cd5173c3f372a",
+    "url": "/AQr/static/js/main.4f781e00.chunk.js"
+  },
+  {
+    "revision": "454bb3503f6f2038eb90",
+    "url": "/AQr/static/js/runtime~main.a58e02de.js"
+  }
+]);
